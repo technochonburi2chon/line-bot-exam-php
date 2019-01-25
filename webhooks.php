b10d33c7d81b9b8c4320be29015d1c4d9a454670
technochonburi2chon <technochonburi2@gmail.com>
2019-01-25 10:50:49 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -20,10 +20,12 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
+			$messagereturn = $events['events'][0]['message']['text'];
+
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $messagereturn;
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
