e7f30de8107e42a1af05f41d5cd1fcc6b8f64589
technochonburi2chon <technochonburi2@gmail.com>
2019-01-27 23:49:55 +0700

    Update testquery_data.php

--- testquery_data.php
+++ testquery_data.php
@@ -19,25 +19,91 @@
 
 												echo "<tr><td>".$result["name_superintendent"]."</td>";
 
+											////เริ่มเงื่อนไขแยกตามเวลา
 												echo "<td>";
 														
-														$SQLCREATE_0612 = "select superintendent,message,date_time_send,group_id from information2_bot where date_time_send BETWEEN '2019-01-25 00:06:00' AND '2019-01-25 00:09:59' AND superintendent = '".$result["id_of_superintendent"]."' ORDER BY date_time_send ASC";
-														$queryResult0612 = mysqli_query($link,$SQLCREATE_0612);		
-														$num_run_rows0612 = mysqli_num_rows($queryResult0612);
+														$SQLCREATE_0609 = "select superintendent,message,date_time_send,group_id from information2_bot where date_time_send BETWEEN '2019-01-25 00:06:00' AND '2019-01-25 00:09:59' AND superintendent = '".$result["id_of_superintendent"]."' ORDER BY date_time_send ASC";
+														$queryResult0609 = mysqli_query($link,$SQLCREATE_0609);		
+														$num_run_rows0609 = mysqli_num_rows($queryResult0609);
 
-														echo "<br>".$SQLCREATE_0612."<br>";
+														//echo "<br>".$SQLCREATE_0612."<br>";
 														
-														if($num_run_rows0612>0)
+														if($num_run_rows0609>0)
 														{
 															echo "<table>";
-																while($result0612 = mysqli_fetch_array($queryResult0612,MYSQLI_ASSOC))
+																while($result0609 = mysqli_fetch_array($queryResult0609,MYSQLI_ASSOC))
 																{
-																	echo "<tr><td>".$result0612["message"]."</td></tr>";
+																	echo "<tr><td>".$result0609["message"]."</td></tr>";
 																}
 															echo "</table>";
 														}
 
 												echo "</td>";
+
+
+												echo "<td>";
+														
+														$SQLCREATE_0912 = "select superintendent,message,date_time_send,group_id from information2_bot where date_time_send BETWEEN '2019-01-25 00:09:00' AND '2019-01-25 00:12:59' AND superintendent = '".$result["id_of_superintendent"]."' ORDER BY date_time_send ASC";
+														$queryResult0912 = mysqli_query($link,$SQLCREATE_0912);		
+														$num_run_rows0912 = mysqli_num_rows($queryResult0912);
+
+														//echo "<br>".$SQLCREATE_0612."<br>";
+														
+														if($num_run_rows0912>0)
+														{
+															echo "<table>";
+																while($result0912 = mysqli_fetch_array($queryResult0912,MYSQLI_ASSOC))
+																{
+																	echo "<tr><td>".$result0912["message"]."</td></tr>";
+																}
+															echo "</table>";
+														}
+
+												echo "</td>";
+
+												echo "<td>";
+														
+														$SQLCREATE_1215 = "select superintendent,message,date_time_send,group_id from information2_bot where date_time_send BETWEEN '2019-01-25 00:12:00' AND '2019-01-25 00:15:59' AND superintendent = '".$result["id_of_superintendent"]."' ORDER BY date_time_send ASC";
+														$queryResult1215 = mysqli_query($link,$SQLCREATE_1215);		
+														$num_run_rows1215 = mysqli_num_rows($queryResult1215);
+
+														//echo "<br>".$SQLCREATE_0612."<br>";
+														
+														if($num_run_rows1215>0)
+														{
+															echo "<table>";
+																while($result1215 = mysqli_fetch_array($queryResult1215,MYSQLI_ASSOC))
+																{
+																	echo "<tr><td>".$result1215["message"]."</td></tr>";
+																}
+															echo "</table>";
+														}
+
+												echo "</td>";
+
+												echo "<td>";
+														
+														$SQLCREATE_1518 = "select superintendent,message,date_time_send,group_id from information2_bot where date_time_send BETWEEN '2019-01-25 00:15:00' AND '2019-01-25 00:18:59' AND superintendent = '".$result["id_of_superintendent"]."' ORDER BY date_time_send ASC";
+														$queryResult1518 = mysqli_query($link,$SQLCREATE_1518);		
+														$num_run_rows1518 = mysqli_num_rows($queryResult1518);
+
+														//echo "<br>".$SQLCREATE_0612."<br>";
+														
+														if($num_run_rows1518>0)
+														{
+															echo "<table>";
+																while($result1518 = mysqli_fetch_array($queryResult1518,MYSQLI_ASSOC))
+																{
+																	echo "<tr><td>".$result1518["message"]."</td></tr>";
+																}
+															echo "</table>";
+														}
+
+												echo "</td>";
+
+
+
+												/////จบเงื่อนไขแยกตามเวลา
 											
 
 											echo "</tr>";
