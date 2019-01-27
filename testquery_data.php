d189fb129d9b6abceb4cd2d6ad87caccd6d4dd9c
technochonburi2chon <technochonburi2@gmail.com>
2019-01-27 23:38:36 +0700

    Update testquery_data.php

--- testquery_data.php
+++ testquery_data.php
@@ -10,14 +10,36 @@
 						$num_run_rows = mysqli_num_rows($queryResultinsertSetdata);
 					
 						
-						
 						if ($num_run_rows>0) 
 						{
 							echo "<table>";
-								echo "<tr><th>ID</th><th>ยศ ชื่อ สกุล</th><th>สังกัด</th></tr>";
+								echo "<tr><th>สถานีตำรวจ</th><th>06:00 น.-09:00 น.</th><th>09:00 น. - 12:00 น.</th><th>12:00 น. - 15:00 น.</th><th>15:00 น. - 18:00 น.</th></tr>";
 										while($result = mysqli_fetch_array($queryResultinsertSetdata,MYSQLI_ASSOC))
 										{
-												echo "<tr><td>".$result["id_of_superintendent"]."</td><td>".$result["name_superintendent"]."</td><td>".$result["police_station"]."</td>";
+
+												echo "<tr><td>".$result["name_superintendent"]."</td>";
+
+												echo "<td>";
+														
+														$SQLCREATE_0612 = "select superintendent,message,date_time_send,group_id from information2_bot where date_time_send BETWEEN '2019-01-25 00:06:00' AND '2019-01-25 00:09:59' AND superintendent = '".$result["id_of_superintendent"]."' ORDER BY date_time_send ASC";
+														$queryResult0612 = mysqli_query($link,$SQLCREATE_0612);		
+														$num_run_rows0612 = mysqli_num_rows($queryResult0612);
+
+														if($num_run_rows0612>0)
+														{
+															echo "<table>";
+																while($result0612 = mysqli_fetch_array($queryResult0612,MYSQLI_ASSOC))
+																{
+																	echo "<tr><td>".$result0612["message"]."</td></tr>";
+																}
+															echo "</table>";
+														}
+
+												echo "</td>";
+											
+
+											echo "</tr>";
+
 										}
 							echo "</table>";
 						}
